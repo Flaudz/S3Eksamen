1fc603a1244cb502287b6129d2cf68cc13d903f9
Nicolaj Thygesen <nico936d@edu.campusvejle.dk>
2021-05-04 14:39:31 +0200

    Error

--- ViewModels/LoginViewModel.cs
+++ ViewModels/LoginViewModel.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using S3Eksamen.Models;
+using S3Eksamen.ViewModels.Commands;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +11,19 @@ namespace S3Eksamen.ViewModels
 {
     public class LoginViewModel
     {
+        private UserModel user;
+        private LoginCommand loginCommand;
+        public UserModel User { get => user; set => user = value; }
+        internal LoginCommand LoginCommand { get => loginCommand; set => loginCommand = value; }
+
+        public LoginViewModel()
+        {
+            this.loginCommand = new LoginCommand(this);
+        }
+
+        public void Login()
+        {
+            Debug.WriteLine(User.UserName);
+        }
     }
 }
